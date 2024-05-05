9334687a88dc0c579693962b11b43997963a855d
LSTV <lstv@mail.com>
2024-05-05 23:34:26 +0200

    asd

--- s/src/ls-js/deprecated/menubar.js
+++
@@ -1,9 +0,0 @@
-{
-    return(_this)=>class Menubar{
-        constructor(id,e=O("menubar"),options={}){
-            if(!e)throw"No element found for menubar.";
-            this.element=O(e);
-
-        }
-    }
-}
