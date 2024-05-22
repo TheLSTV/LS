8aa38248fd98991b0e7806950a0706dd60e790f0
LSTV <lstv@mail.com>
2024-05-22 20:40:29 +0200

    Update knob.js

--- ls-js/knob.js
+++ ls-js/knob.js
@@ -3,21 +3,25 @@
         default: {
             pointer: "dot"
         },
+
         chrome: {
             arcFill: false,
             arcWidth: 5,
             pointer: "line",
             pointerGlow: true
         },
+
         flat: {
             arcFill: false,
             arcBackground: true
         },
+
         progress: {
             arcGap: [180, 540],
             arcFill: false
         }
     }
+
     gl.defaultStyles = {
         arcGap: [220, 500],
         arc: true,
@@ -28,6 +32,7 @@
         arcFill: true,
         pointer: "none"
     }
+
     function describeArc(x, y, radius, spread, startAngle, endAngle, fill){
         function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
             var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
@@ -57,7 +62,8 @@
 
         return d;
     }
-    return _this=>class LS_Knob{
+
+    return _this => class LS_Knob{
         constructor (id, element, style = "default", options = {}) {
             _this = this;
 
@@ -78,6 +84,7 @@
             }
             
             this.svg = this.element.get("svg");
+
             this.svg.attrAssign({
                 width: 200,
                 height: 200,
@@ -92,7 +99,20 @@
                 this.element.add(N({class:"ls-knob-stator"}))
             }
 
-            this.value = 0;
+            let _value = 0;
+
+            Object.defineProperties(this, {
+                value: {
+                    get(){
+                        return _value
+                    },
+
+                    set(value){
+                        _value = Math.max(_this.options.min, Math.min(value, _this.options.max))
+                        _this.draw()
+                    }
+                }
+            })
 
             let handle = LS.Util.RegisterMouseDrag(this.element),
                 isDragging = false
@@ -103,14 +123,15 @@
             this.enabled = true;
 
             this.updateStyle()
-            
+
             handle.on("start", ()=>{
                 _this.drawInit();
                 _this.draw();
+
                 this.element.requestPointerLock();
             })
 
-            handle.on("move", (x,y,e)=>_this.draw(e))
+            handle.on("move", (x, y, e) => _this.event(e))
             
             handle.on("end", ()=>{
                 if (isDragging) {
@@ -127,15 +148,18 @@
                 }
             });
         }
-        draw(e){
-            let step = e? e.movementY * _this.options.step : 0;
-            if(_this.value - step < _this.options.min || _this.value - step > _this.options.max){
-                return
-            }
+
+        event(event) {
+            let step = event? event.movementY * _this.options.step : 0;
+
             _this.value -= step;
+        }
+
+        draw() {
             _this.percentage = ((_this.value - _this.options.min) / (_this.options.max - _this.options.min)) * 100;
             _this.arcAngle = (_this.style.arcGap[0] + (_this.percentage / 100) * (_this.style.arcGap[1] - _this.style.arcGap[0]));
-            if(_this.invoke)_this.invoke("changed", _this.value)
+            
+            if(_this.invoke) _this.invoke("changed", _this.value)
 
 
             if(_this.style.pointer !== "none"){
@@ -146,27 +170,35 @@
                 _this.arc.setAttribute("d", _this.Arc(_this.style.arcFill))
             }
         }
+
         drawInit(){
             if(_this.style.pointer !== "none"){
+
                 if(!_this.rotor){
                     _this.rotor = N({class:"ls-knob-rotor"});
                     _this.element.get(".ls-knob-stator").add(_this.rotor)
                 }
                 _this.rotor.show()
-            }else if(_this.rotor){
+
+            } else if(_this.rotor) {
                 _this.rotor.hide()
             }
+
             if(_this.style.arc){
                 if(!_this.arc){
                     _this.arc = O(D().createElementNS("http://www.w3.org/2000/svg", "path"))
                     _this.arc.class("ls-knob-arc")
                     _this.svg.add(_this.arc)
                 }
+
                 if(_this.style.arcFill){
+
                     _this.arc.attrAssign({
                         fill: "var(--accent)"
                     })
-                }else{
+
+                } else {
+
                     _this.arc.attrAssign({
                         fill: "transparent",
                         stroke: "var(--accent)",
@@ -175,39 +207,53 @@
                     })
                     _this.element.style.setProperty("--knob-stroke-width", _this.element.getBoundingClientRect().height * (_this.style.arcWidth/100) + "px")
                 }
+
                 _this.arc.show()
-            }else if(_this.arc){_this.arc.hide()}
+            } else if (_this.arc) {
+                _this.arc.hide()
+            }
+
             if(_this.style.arcBackground && !_this.back){
                 _this.back = O(D().createElementNS("http://www.w3.org/2000/svg", "path"));
                 _this.back.setAttribute("fill", "transparent")
                 _this.arc.addBefore(_this.back)
             }
+
             if(_this.style.arcBackground){
                 _this.back.class("ls-knob-arc-full")
                 _this.back.setAttribute("d", _this.Arc(false, _this.style.arcGap[1]))
             }
+
             _this.element.get(".ls-knob-stator").class("ls-knob-glow", _this.style.pointerGlow ? 1 : 0)
         }
+
         setStyle(style, quiet = false){
             if(typeof style === "string"){
                 let styleName = style;
                 style = gl.presets[style] || {}
                 style.name = styleName
             }
+        
             _this.style = {...gl.defaultStyles, ...style};
-            if(!quiet)_this.updateStyle()
+
+            if(!quiet) _this.updateStyle()
         }
+
         updateStyle(opt = {}){
             Object.assign(_this.style, opt)
             _this.element.attrAssign({"knob-preset": _this.style.name})
             _this.element.attrAssign({"knob-pointer": _this.style.pointer})
             _this.updateOptions()
         }
+
         updateOptions(opt = {}){
             Object.assign(_this.options, opt)
             _this.drawInit()
             _this.draw()
+
+            setTimeout(() => _this.drawInit(), 10)
         }
+
         Arc(fill = true, ang = _this.arcAngle){
             if( ang % 180 == 0 ) ang-- ; // Lazy fix
             return describeArc(100, 100, _this.style.arcSpread, 100 - (_this.style.arcFill? 0 : _this.style.arcWidth), _this.style.arcGap[0], ang, fill)
